eb93c5ab96695ff42ca0aff8cc84da3ffd5f6be3
Raunaq Saxena <raunaqsaxena23@gmail.com>
2016-11-07 14:20:04 -0500

    First commit

--- src/sim_bp.java
+++ src/sim_bp.java
@@ -2,4 +2,37 @@
  * Created by raunaq on 11/7/16.
  */
 public class sim_bp {
+    public static void main(String[] args)
+    {
+        int argLength;
+        String predictorType="";
+
+        argLength=args.length();
+
+        if (argLength==0)
+        {
+            System.out.println("Not enough arguments");
+            System.exit(0);
+        }
+
+        predictorType = args[0];
+
+        if(predictorType.equals("bimodal") && argLength>4)
+        {
+
+        }
+        else if(predictorType.equals("bimodal") && argLength>4)
+        {
+
+        }
+        else if(predictorType.equals("bimodal") && argLength>4)
+        {
+
+        }
+        else
+        {
+
+        }
+
+    }
 }
