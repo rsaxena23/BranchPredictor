08dc5c6ad52da3f149ecb387f757adb31600893a
Raunaq Saxena <raunaqsaxena23@gmail.com>
2016-11-08 21:00:10 -0500

    Added Hybrid and printFormat

--- src/Predictor.java
+++ src/Predictor.java
@@ -4,6 +4,7 @@
 public class Predictor {
     int hValue;
     int iValue;
+    int kValue;
     long globalHvalue = 0;
 
     int totalPredictions=0;
@@ -12,6 +13,8 @@ public class Predictor {
     short predictArray[];
 
     int predictorType=0;
+
+    Predictor gPred,bPred;
 
     public Predictor(int iValue,int hValue,int predictorType)
     {
@@ -23,6 +26,17 @@ public class Predictor {
         //initialize the Predict Table
         for(int i=0;i<predictArray.length;i++)
             predictArray[i]=2;
+    }
+    public Predictor(int kValue, int predictorType, Predictor gPred, Predictor bPred)
+    {
+        this.predictorType = predictorType;
+        this.kValue = kValue;
+        this.gPred = gPred;
+        this.bPred = bPred;
+        this.predictArray = new short[(int)Math.pow(2,kValue)];
+
+        for(int i=0;i<predictArray.length;i++)
+            predictArray[i]=1;
     }
 
     public long getTranslatedValue(String address)
@@ -54,7 +68,7 @@ public class Predictor {
 
     }
 
-    public boolean predict(String address,boolean outcome)
+    public boolean predict(String address,boolean outcome, int predictorType)
     {
         int index = (int)getTranslatedValue(address);
         boolean result;
@@ -66,17 +80,62 @@ public class Predictor {
             result = true;
         else
             result = false;
+        if(predictorType!=Constants.HYBRID) {
+            if (outcome)
+                predictArray[index] += (predictArray[index] < 3) ? 1 : 0;
+            else
+                predictArray[index] -= (predictArray[index] > 0) ? 1 : 0;
+        }
+
+        if(hValue>0)
+            updateHValue((outcome)?1:0);
+
+        return result;
+    }
+
+    public boolean predictHybrid(String address,boolean outcome)
+    {
+        long gIndex = gPred.getTranslatedValue(address);
+        boolean gPrediction = gPred.predict(address,outcome,predictorType);
+        boolean bPrediction = bPred.predict(address,outcome,predictorType);
+        boolean result;
+        long addr = Long.parseLong(address,16);
+        int kIndex =(int)((addr>>2)&((long)(Math.pow(2,kValue)-1)));
+        //System.out.println("\n"+address+" >>2 "+Long.toHexString(addr>>2)+" "+Long.toHexString(kIndex));
+        //System.out.println(" index:"+kIndex+" choice:"+predictArray[kIndex]+" g:"+gPrediction+" b:"+bPrediction+" res:"+outcome);
+
+
+
+        if(predictArray[kIndex]>=2) {
+            gPred.updatePredictTable((int) gIndex, outcome);
+            result = gPrediction;
+        }
+        else {
+            bPred.updatePredictTable((int) bPred.getTranslatedValue(address), outcome);
+            result = bPrediction;
+        }
+
+        if(gPrediction!=bPrediction)
+        {
+            if(gPrediction==outcome)
+                predictArray[kIndex]+= (predictArray[kIndex]<3)?1:0;
+            else
+                predictArray[kIndex]-= (predictArray[kIndex]>0)?1:0;
+        }
+        //System.out.println("after:"+predictArray[kIndex]+" ghr:"+gPred.globalHvalue);
+        return result;
+    }
+
+    public void updatePredictTable(int index,boolean outcome)
+    {
+        //System.out.println("array before:"+predictArray[index]);
 
         if (outcome)
             predictArray[index]+= (predictArray[index]<3)?1:0;
         else
             predictArray[index]-= (predictArray[index]>0)?1:0;
 
-        if(hValue>0)
-            updateHValue((outcome)?1:0);
-
-        return result;
-
+        //System.out.println("array after:"+predictArray[index]);
     }
 
 }
--- src/sim_bp.java
+++ src/sim_bp.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 
 public class sim_bp {
 
+    int totalPredictions = 0;
+
     public void startSimulate(BTB btbCache, Predictor pred, String traceFile)
     {
         String line,values[],address="",btbTranslate[];
@@ -19,6 +21,7 @@ public class sim_bp {
             BufferedReader br = new BufferedReader(new FileReader(traceFile));
             while((line=br.readLine())!=null)
             {
+                totalPredictions+=1;
                 values = line.split(" ");
                 address = values[0];
                 outcome = values[1].equals("t");
@@ -34,14 +37,18 @@ public class sim_bp {
                 if(checkPredictor)
                 {
                     pred.totalPredictions+=1;
-                    predOutcome = pred.predict(address,outcome);
+                    if(pred.predictorType!=Constants.HYBRID)
+                        predOutcome = pred.predict(address,outcome,pred.predictorType);
+                    else
+                        predOutcome = pred.predictHybrid(address,outcome);
+
                     if(predOutcome!=outcome)
                         pred.misses+=1;
                     else
                         pred.hits+=1;
                 }
                 /*counter++;
-                if (counter>20)
+                if (counter>100)
                     break;*/
             }
 
@@ -55,23 +62,76 @@ public class sim_bp {
 
     public void printResults(Predictor pred, BTB btbCache)
     {
-        if(pred.predictorType==Constants.GSHARE)
-            System.out.println("Final GHR value:"+pred.globalHvalue+" :"+Long.toHexString(pred.globalHvalue));
+        int a,b,c,d,i,j;
+        double e;
+        TagEntry tempEntry;
+        a=totalPredictions;
+        b=pred.totalPredictions;
+        c=pred.misses;
+        d=(btbCache!=null)?btbCache.misses:0;
+        e=(double)(c+d)/(double)a;
 
-        System.out.println("Number of predictions from the branch predictor: "+pred.totalPredictions);
-        System.out.println("Number of mispredictions from the branch predictor: "+pred.misses);
+        if(btbCache!=null)
+        {
+            System.out.println("\nFinal BTB Tag Array Contents {valid, pc}:");
+            for(i=0;i<btbCache.tagArray.length;i++)
+            {
+                System.out.print("Set "+i+": ");
+                for(j=0;j<btbCache.assoc;j++)
+                {
+                    tempEntry = btbCache.tagArray[i][j];
+                    System.out.print(" {"+((tempEntry.valid)?1:0)+", 0x "+tempEntry.value+"} ");
+                }
+                System.out.println();
+            }
 
-        if(btbCache!=null) {
-            System.out.println("Number of mispredictions from the BTB predictor: "+btbCache.misses);
         }
+
+        if(pred.predictorType==Constants.HYBRID)
+        {
+            System.out.println("\nFinal Bimodal Table Contents:");
+            for(i=0;i<pred.bPred.predictArray.length;i++)
+                System.out.println("table["+i+"]: "+pred.bPred.predictArray[i]);
+
+            System.out.println("\nFinal Gshare Table Contents:");
+            for(i=0;i<pred.gPred.predictArray.length;i++)
+                System.out.println("table["+i+"]: "+pred.gPred.predictArray[i]);
+            System.out.println("Final GHR Contents: 0x "+Long.toHexString(pred.globalHvalue));
+
+            System.out.println("\nFinal Chooser Table Contents:");
+            for(i=0;i<pred.predictArray.length;i++)
+                System.out.println("Choice table["+i+"]: "+pred.predictArray[i]);
+        }
+        else
+        {
+            String predName;
+            if(pred.predictorType==Constants.BIMODIAL)
+                predName="Bimodal";
+            else
+                predName="Gshare";
+
+            System.out.println("\nFinal "+predName+" Table Contents:");
+            for(i=0;i<pred.predictArray.length;i++)
+                System.out.println("table["+i+"]: "+pred.predictArray[i]);
+        }
+
+        if(pred.predictorType==Constants.GSHARE)
+            System.out.println("Final GHR Contents: 0x "+Long.toHexString(pred.globalHvalue));
+
+        System.out.println("\nFinal Branch Predictor Statistics:");
+        System.out.println("a. Number of branches: "+a);
+        System.out.println("b. Number of predictions from the branch predictor: "+b);
+        System.out.println("c. Number of mispredictions from the branch predictor: "+c);
+        System.out.println("d. Number of mispredictions from the BTB predictor: "+d);
+        System.out.println("e. Branch misprediction rate: "+(Math.round(e*100)/100.0));
     }
 
     public static void main(String[] args)
     {
-        int argLength,hValue=0,indexPred=0,indexBTB=0,assocBTB=0;
+        int argLength,hValue=0,kValue=0,indexPred=0,indexBTB=0,assocBTB=0,indexPredBimodal=0;
         String predictorType="",traceFile="";
         BTB btbCache = null;
-        Predictor pred = null;
+        Predictor pred = null,gPred=null,bPred=null;
 
         argLength=args.length;
 
@@ -95,6 +155,8 @@ public class sim_bp {
             if ( indexBTB!=0 && assocBTB!=0 )
                 btbCache = new BTB(indexBTB,assocBTB);
 
+            System.out.println("Command Line\n./sim_bp "+args[0]+" "+args[1]+" "+args[2]+" "+args[3]+" "+args[4]);
+
         }
         else if(predictorType.equals("gshare") && argLength>5)
         {
@@ -111,9 +173,29 @@ public class sim_bp {
             if ( indexBTB!=0 && assocBTB!=0 )
                 btbCache = new BTB(indexBTB,assocBTB);
 
+            System.out.println("Command Line\n./sim_bp "+args[0]+" "+args[1]+" "+args[2]+" "+args[3]+" "+args[4]+" "+args[5]);
+
         }
         else if(predictorType.equals("hybrid") && argLength>6)
         {
+            kValue = Integer.parseInt(args[1]);
+            indexPred = Integer.parseInt(args[2]);
+            hValue = Integer.parseInt(args[3]);
+            indexPredBimodal = Integer.parseInt(args[4]);
+            indexBTB = Integer.parseInt(args[5]);
+            assocBTB = Integer.parseInt(args[6]);
+            traceFile = args[7];
+
+            gPred = new Predictor(indexPred,hValue,Constants.GSHARE);
+            bPred = new Predictor(indexPredBimodal,0,Constants.BIMODIAL);
+            pred = new Predictor(kValue, Constants.HYBRID, gPred, bPred );
+
+            //System.out.println("i: "+indexPred+" h:"+hValue);
+
+            if ( indexBTB!=0 && assocBTB!=0 )
+                btbCache = new BTB(indexBTB,assocBTB);
+
+            System.out.println("Command Line\n./sim_bp "+args[0]+" "+args[1]+" "+args[2]+" "+args[3]+" "+args[4]+" "+args[5]+" "+args[6]+" "+args[7]);
 
         }
         else
@@ -125,6 +207,5 @@ public class sim_bp {
         sim_bp obj = new sim_bp();
         obj.startSimulate(btbCache,pred,traceFile);
         obj.printResults(pred,btbCache);
-
     }
 }
